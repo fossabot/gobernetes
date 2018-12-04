9effb601fbf194dc6b65049daacb84857b28a730
Aracki <aracki.ivan@gmail.com>
2018-12-04 11:20:26 +0100

    Add removing the go binary

--- build.sh
+++ build.sh
@@ -2,4 +2,5 @@
 
 GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -a -o app/go-web-server app/go-web-server.go
 docker build --no-cache -t aracki/go-web-server app/
+rm app/go-web-server
 docker push aracki/go-web-server
