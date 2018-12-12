3ad3c9856e0c4a634bdfae29822e09e662337e1e
doza-daniel <doza.daniel@gmail.com>
2018-12-12 18:20:59 +0100

    new route in webserver: memleak. start a memory leaking process which will not stop until out of memory

--- app/go-web-server.go
+++ app/go-web-server.go
@@ -1,19 +1,24 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 	"os/exec"
 	"runtime"
+	"strconv"
+	"time"
 )
 
 func main() {
 	http.HandleFunc("/", hello)
 	http.HandleFunc("/write", write)
 	http.HandleFunc("/cat", cat)
+	http.HandleFunc("/memleak", memleak)
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
@@ -54,4 +59,48 @@ func cat(w http.ResponseWriter, r *http.Request) {
 	}
 
 	fmt.Fprintf(w, "Output from file %s: \n %s", tempFile, string(out))
+}
+
+// memleak example route hit: http://localhost:8080/memleak?megabytes=123&interval=1000
+func memleak(w http.ResponseWriter, r *http.Request) {
+	var howMany, interval int64 = 100, 350
+	mbs := r.URL.Query().Get("megabytes")
+	if mbs != "" {
+		var err error
+		howMany, err = strconv.ParseInt(mbs, 10, 64)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+	}
+	ms := r.URL.Query().Get("interval")
+	if mbs != "" {
+		var err error
+		interval, err = strconv.ParseInt(ms, 10, 64)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+	}
+
+	file, err := os.Open("/dev/urandom")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	go func() {
+		buff := new(bytes.Buffer)
+
+		for {
+			if _, err := io.CopyN(buff, file, howMany*1e6); err != nil {
+				log.Printf("copyN failed: %v\n", err)
+				log.Println("memleak stopping...")
+				return
+			}
+			time.Sleep(time.Duration(interval) * time.Millisecond)
+		}
+	}()
+
+	fmt.Fprintf(w, "memleak running: %dMB / %dms\n", howMany, interval)
 }
