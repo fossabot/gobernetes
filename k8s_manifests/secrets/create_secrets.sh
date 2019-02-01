df738b8bf9aa8f6d51daee81b8a17b6a4c02db99
Aracki <aracki.ivan@gmail.com>
2019-02-01 19:54:47 +0100

    Add GF_SECURITY_ADMIN_PASSWORD; create secrets folder

--- k8s_manifests/secrets/create_secrets.sh
+++ k8s_manifests/secrets/create_secrets.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-# create secret for alert manager
+# ALERT MANAGER SLACK RECEIVER
 kubectl create -n monitoring secret generic alertmanager-main --from-file=monitoring/secrets/alertmanager.yaml
 
-# create secret for accessing route53
+# ROUTE53 ACCESS
 kubectl create -n cert-manager secret generic route53 --from-file=secret_key
 
-# create ConfigMap with modified traefik.toml
+# GRAFANA AUTHENTICATION
+kubectl create -n monitoring secret generic grafana-auth --from-file=grafana-auth
+
+# TRAEFIK.TOML recreation
 kubectl delete -n traefik configmap traefik-conf
 kubectl create -n traefik configmap traefik-conf --from-file=ingress-controller-traefik/deployment_tls_ready/traefik.toml
+
