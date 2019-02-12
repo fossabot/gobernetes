75da6f0688859c6baf5bcdf0fdd763a7366c603b
Aracki <aracki.ivan@gmail.com>
2019-02-12 14:40:48 +0100

    Add traefik.toml as ConfigMap resource

--- k8s_manifests/create_secrets.sh
+++ k8s_manifests/create_secrets.sh
@@ -9,8 +9,3 @@ kubectl create -n cert-manager secret generic route53 --from-file=secrets/secret
 
 # GRAFANA AUTHENTICATION
 kubectl create -n monitoring secret generic grafana-auth --from-file=secrets/grafana-auth
-
-# TRAEFIK.TOML recreation
-kubectl delete -n traefik configmap traefik-conf
-kubectl create -n traefik configmap traefik-conf --from-file=ingress-controller-traefik/deployment_tls_ready/traefik.toml
-
