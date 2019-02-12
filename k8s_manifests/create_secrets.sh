ab364c3c81dd170defe50933654c93067279e6f6
Aracki <aracki.ivan@gmail.com>
2019-02-12 14:03:49 +0100

    Move create_secrets.sh to root k8s_manifests

---
+++ k8s_manifests/create_secrets.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# ALERT MANAGER SLACK RECEIVER recreation
+kubectl delete -n monitoring secret alertmanager-main
+kubectl create -n monitoring secret generic alertmanager-main --from-file=secrets/alertmanager.yaml
+
+# ROUTE53 ACCESS
+kubectl create -n cert-manager secret generic route53 --from-file=secrets/secret_key
+
+# GRAFANA AUTHENTICATION
+kubectl create -n monitoring secret generic grafana-auth --from-file=secrets/grafana-auth
+
+# TRAEFIK.TOML recreation
+kubectl delete -n traefik configmap traefik-conf
+kubectl create -n traefik configmap traefik-conf --from-file=ingress-controller-traefik/deployment_tls_ready/traefik.toml
+
