605703677fb2a041fe805c43f76a0e97d71cb73e
Aracki <aracki.ivan@gmail.com>
2019-02-12 16:21:04 +0100

    Rename aws_secret_key

--- k8s_manifests/create_secrets.sh
+++ k8s_manifests/create_secrets.sh
@@ -5,7 +5,7 @@ kubectl delete -n monitoring secret alertmanager-main
 kubectl create -n monitoring secret generic alertmanager-main --from-file=secrets/alertmanager.yaml
 
 # ROUTE53 ACCESS
-kubectl create -n cert-manager secret generic route53 --from-file=secrets/secret_key
+kubectl create -n cert-manager secret generic route53 --from-file=secrets/aws_secret_key
 
 # GRAFANA AUTHENTICATION
 kubectl create -n monitoring secret generic grafana-auth --from-file=secrets/grafana-auth
